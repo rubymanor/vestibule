0beef2aecad0760c56e065d8936b1f3504504c81
Murray Steele <muz@h-lame.com>
2010-10-16 15:30:58 +0100

    Oh. It's a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vestibule::Application.config.secret_token = '9acafaf4c518a31c574b200cfb0ab28556ebdedcfb88fb40147e3bc4fd0bdc29b93c14e474742782c2e2d31bfe951f53e602514dfeb628304b763161bd4a65ac'
