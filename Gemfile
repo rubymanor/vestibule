09772cde0ea642e5af52685d876ad56941fc7159
Murray Steele <muz@h-lame.com>
2010-10-17 00:04:34 +0100

    Adding Capybara and Database Cleaner to the dependencies and spinning up our own integration test case (with DSL enhancements) to use them.

--- Gemfile
+++ Gemfile
@@ -7,4 +7,6 @@ group :test do
   gem "factory_girl_rails", "1.0"
   gem "shoulda", "2.11.3"
   gem "ruby-debug19"
+  gem "capybara", "0.3.9"
+  gem "database_cleaner", "0.5.2"
 end
---
+++ test/support/integration_test_case.rb
@@ -0,0 +1,35 @@
+require 'capybara/rails'
+require 'capybara/dsl'
+
+class IntegrationTestCase < ActiveSupport::TestCase
+  include Capybara
+  include ActionDispatch::Assertions
+  include Rails.application.routes.url_helpers
+  self.default_url_options[:host] = 'http://example.com/'
+
+  self.use_transactional_fixtures = false
+
+  setup do
+    Capybara.default_selector = :css
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
+
+  def i_am_on(expected_path)
+    assert_equal expected_path, current_path
+  end
+
+  def the_page_has_title(expected_title)
+    ['h1', 'title'].each do |title_selector|
+      within(title_selector) do
+        debugger
+        assert page.has_content?(expected_title), %Q{Expected "#{expected_title}" within CSS selector '#{title_selector}', but it's not there}
+      end
+    end
+  end
+end
