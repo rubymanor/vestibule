da42d42b5cea42749ea226d26dfac2d87d33b750
Murray Steele <muz@h-lame.com>
2010-12-15 21:57:18 +0000

    Use gmail for smtp in production. We're adding the username and password as ENV vars in heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,14 @@ Vestibule::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'rubymanor.org' }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'rubymanor.org',
+    :user_name            => ENV['GMAIL_SMTP_USER'],
+    :password             => ENV['GMAIL_SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
