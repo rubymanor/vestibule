86b89cbda7777830c00b38cf5927dd01004905d0
Murray Steele <muz@h-lame.com>
2010-12-16 01:09:39 +0000

    Adding some useful scopes to the contribution model.

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,4 +4,8 @@ class Contribution < ActiveRecord::Base
 
   validates :talk, :user, :presence => true
   validates :kind, :inclusion => ['suggest', 'provide extra detail']
+
+  scope :providing_extra_detail, where(:kind => 'provide extra detail')
+  scope :suggestions, where(:kind => 'suggest')
+  scope :by_user, lambda { |u| where(:user_id => u.id) }
 end
--- test/unit/contribution_test.rb
+++ test/unit/contribution_test.rb
@@ -43,4 +43,72 @@ class ContributionTest < ActiveSupport::TestCase
       end
     end
   end
+
+  context "providing_extra_detail" do
+    setup do
+      @c1 = Factory.create(:contribution, :kind => 'provide extra detail')
+      @c2 = Factory.create(:contribution, :kind => 'suggest')
+      @c3 = Factory.create(:contribution, :kind => 'provide extra detail')
+    end
+
+    should 'not return any contributions that are of kind \'suggest\'' do
+      assert !Contribution.providing_extra_detail.include?(@c2)
+    end
+
+    should 'return all contributions that are of kind \'provide extra detail\'' do
+      assert Contribution.providing_extra_detail.include?(@c1)
+      assert Contribution.providing_extra_detail.include?(@c3)
+    end
+
+    should 'be empty when there are no suggestions' do
+      Contribution.delete_all(:kind => 'provide extra detail')
+      assert_empty Contribution.providing_extra_detail
+    end
+  end
+
+  context "suggestions" do
+    setup do
+      @c1 = Factory.create(:contribution, :kind => 'suggest')
+      @c2 = Factory.create(:contribution, :kind => 'provide extra detail')
+      @c3 = Factory.create(:contribution, :kind => 'suggest')
+    end
+
+    should 'not return any contributions that are of kind \'provide extra detail\'' do
+      assert !Contribution.suggestions.include?(@c2)
+    end
+
+    should 'return all contributions that are of kind \'suggest\'' do
+      assert Contribution.suggestions.include?(@c1)
+      assert Contribution.suggestions.include?(@c3)
+    end
+
+    should 'be empty when there are no suggestions' do
+      Contribution.delete_all(:kind => 'suggest')
+      assert_empty Contribution.suggestions
+    end
+  end
+
+  context "by_user" do
+    setup do
+      @u1 = Factory.create(:user)
+      @u2 = Factory.create(:user)
+      @u3 = Factory.create(:user)
+      @c1 = Factory.create(:contribution, :user => @u1)
+      @c2 = Factory.create(:contribution, :user => @u2)
+      @c3 = Factory.create(:contribution, :user => @u1)
+    end
+
+    should 'not return contributions for different users' do
+      assert !Contribution.by_user(@u1).include?(@c2)
+    end
+
+    should 'return all contributions for the given user' do
+      assert Contribution.by_user(@u1).include?(@c1)
+      assert Contribution.by_user(@u1).include?(@c3)
+    end
+
+    should 'be empty if the user has made no contributions' do
+      assert_empty Contribution.by_user(@u3)
+    end
+  end
 end
