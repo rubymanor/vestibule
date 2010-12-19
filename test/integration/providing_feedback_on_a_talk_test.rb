389b4b1b01839cda71506c4b3840bd8a52a67a0d
Murray Steele <muz@h-lame.com>
2010-12-19 20:34:42 +0000

    Move things around somewhat and share info between the index and show pages for talks.
    Also add a count of feedback items to the info we show about a talk.

--- test/integration/providing_feedback_on_a_talk_test.rb
+++ test/integration/providing_feedback_on_a_talk_test.rb
@@ -8,37 +8,72 @@ class ProvidingFeedbackOnATalkTest < IntegrationTestCase
     @feedback_3  = Factory.create(:feedback, :talk => @talk, :created_at => 4.days.ago)
   end
 
-  scenario "When I am not a logged in user I can see existing feedback, but I can't leave any.  I am however, prompted to sign up or login to do so" do
-    visit talk_path(@talk)
+  context "As a visitor" do
+    scenario "When I visit a talk page I can see existing feedback, but I can't leave any.  I am however, prompted to sign up or login to do so" do
+      visit talk_path(@talk)
 
-    within('#feedback') do
-      assert page.has_content?(@feedback_1.content)
-      assert page.has_content?(@feedback_2.content)
-      assert page.has_content?(@feedback_3.content)
-      there_is_no_feedback_form
-      there_are_sign_up_and_sign_in_links
+      within('#feedback') do
+        assert page.has_content?('3 items of feedback')
+        assert page.has_content?(@feedback_1.content)
+        assert page.has_content?(@feedback_2.content)
+        assert page.has_content?(@feedback_3.content)
+        there_is_no_feedback_form
+        there_are_sign_up_and_sign_in_links
+      end
+    end
+
+    scenario "When I visit the talks index, I can see at a glance how much feedback it has" do
+      visit '/'
+
+      within '#talks .talk:first-child' do
+        assert page.has_content?('3 items of feedback')
+      end
     end
   end
 
-  scenario "When I am a logged in user I can see existing feedback and leave some of my own, which appears at the bottom of the list of feedback" do
-    sign_in
-
-    visit talk_path(@talk)
-
-    within('#feedback') do
-      assert page.has_content?(@feedback_1.content)
-      assert page.has_content?(@feedback_2.content)
-      assert page.has_content?(@feedback_3.content)
-      there_are_no_sign_up_and_sign_in_links
-      there_is_a_feedback_form
-
-      fill_in "Feedback", :with => 'This talk sounds really good so far. I\'d like to know if the presenter is going to cover some of the pain points that newbie sometimes feel when getting up and running, and how we might address those?'
-      click_button 'Leave feedback'
+  context "As a signed in user" do
+    setup do
+      sign_in
     end
 
-    i_am_on talk_path(@talk)
-    within('#feedback #feedback_items .feedback:last-child') do
-      assert page.has_content?('This talk sounds really good so far. I\'d like to know if the presenter is going to cover some of the pain points that newbie sometimes feel when getting up and running, and how we might address those?')
+    scenario "When I visit a talk page I can see existing feedback and leave some of my own, which appears at the bottom of the list of feedback" do
+      visit talk_path(@talk)
+
+      within('#feedback') do
+        assert page.has_content?('3 items of feedback')
+        assert page.has_content?(@feedback_1.content)
+        assert page.has_content?(@feedback_2.content)
+        assert page.has_content?(@feedback_3.content)
+        there_are_no_sign_up_and_sign_in_links
+        there_is_a_feedback_form
+
+        fill_in "Feedback", :with => 'This talk sounds really good so far. I\'d like to know if the presenter is going to cover some of the pain points that newbie sometimes feel when getting up and running, and how we might address those?'
+        click_button 'Leave feedback'
+      end
+
+      i_am_on talk_path(@talk)
+      within('#feedback') do
+        assert page.has_content?('4 items of feedback')
+        within('#feedback_items .feedback:last-child') do
+          assert page.has_content?('This talk sounds really good so far. I\'d like to know if the presenter is going to cover some of the pain points that newbie sometimes feel when getting up and running, and how we might address those?')
+        end
+      end
+    end
+
+    scenario "When I visit the talks index, I can see at a glance how much feedback it has" do
+      visit '/'
+      within '#talks .talk:first-child' do
+        assert page.has_content?('3 items of feedback')
+      end
+
+      visit talk_path(@talk)
+      fill_in 'Feedback', :with => 'This talk sounds really good so far. I\'d like to know if the presenter is going to cover some of the pain points that newbie sometimes feel when getting up and running, and how we might address those?'
+      click_button 'Leave feedback'
+
+      visit '/'
+      within '#talks .talk:first-child' do
+        assert page.has_content?('4 items of feedback')
+      end
     end
   end
 
--- test/integration/viewing_talks_test.rb
+++ test/integration/viewing_talks_test.rb
@@ -2,13 +2,19 @@ require 'test_helper'
 
 class ViewingTalksTest < IntegrationTestCase
   setup do
-    @talk_1 = Factory.create(:talk, :title => 'Jumping for joy with <Ruby>!', :created_at => 10.days.ago, :updated_at => 9.days.ago)
+    @suggester = Factory.create(:user)
+    @extra_detail_provider = Factory.create(:user)
+    @discusser = Factory.create(:user)
+
+    @talk_1 = Factory.create(:talk, :title => 'Jumping for joy with <Ruby>!', :created_at => 10.days.ago, :updated_at => 9.days.ago, :suggester => @suggester)
+    @talk_1.contributions.create_providing_extra_detail(:user => @extra_detail_provider)
+    Factory.create(:feedback, :talk => @talk_1, :user => @discusser)
     @talk_2 = Factory.create(:talk, :title => 'Touching RedCloth', :created_at => 1.minute.ago, :updated_at => 1.minute.ago)
     @talk_3 = Factory.create(:talk, :title => 'Pass the salt father, Oh! and by the way, I use Ruby', :created_at => 10.minutes.ago, :updated_at => 10.minutes.ago)
     @talk_4 = Factory.create(:talk, :title => 'To Ruby or not to Ruby', :created_at => 4.days.ago, :updated_at => 2.minutes.ago)
   end
 
-  scenario "I can visit the talks index and see a list of talks that link to individual talk pages" do
+  scenario "When I visit the talks index, I can see a list of talks that link to individual talk pages" do
     visit '/'
 
     the_page_has_title 'Talks'
@@ -27,41 +33,58 @@ class ViewingTalksTest < IntegrationTestCase
       sign_in
     end
 
-    scenario "When viewing the page about a talk I can see all the details of that talk that have been filled in, and am prompted to fill in the missing ones" do
-      visit talk_path(@talk_1)
+    context "When viewing the page about a talk" do
+      setup do
+        visit talk_path(@talk_1)
 
-      the_page_has_title @talk_1.title
+        the_page_has_title @talk_1.title
+      end
 
-      this_section_of_the_talk_page_is_empty 'abstract'
-      i_follow_the_link_in_this_section_and_fill_out_the_missing_detail 'abstract', :with => 'This talk will cover why I think ruby is a joyful language.'
-      this_section_of_the_talk_page_is_not_empty 'abstract', :with_content => 'This talk will cover why I think ruby is a joyful language.'
+      scenario "I can see all the details of that talk that have been filled in, and am prompted to fill in the missing ones" do
+        this_section_of_the_talk_page_is_empty 'abstract'
+        i_follow_the_link_in_this_section_and_fill_out_the_missing_detail 'abstract', with: 'This talk will cover why I think ruby is a joyful language.'
+        this_section_of_the_talk_page_is_not_empty 'abstract', with_content: 'This talk will cover why I think ruby is a joyful language.'
 
-      this_section_of_the_talk_page_is_empty 'outline'
-      i_follow_the_link_in_this_section_and_fill_out_the_missing_detail 'outline', :with => 'The first slides will cover my "Aw yeah!" moment with Ruby. The next slides will be the same thing from some other rubyists. It\'ll finish up with ways you can help other programmers experience their own "Aw yeah!" moments when they first experience ruby.'
-      this_section_of_the_talk_page_is_not_empty 'outline', :with_content => 'The first slides will cover my "Aw yeah!" moment with Ruby. The next slides will be the same thing from some other rubyists. It\'ll finish up with ways you can help other programmers experience their own "Aw yeah!" moments when they first experience ruby.'
+        this_section_of_the_talk_page_is_empty 'outline'
+        i_follow_the_link_in_this_section_and_fill_out_the_missing_detail 'outline', with: 'The first slides will cover my "Aw yeah!" moment with Ruby. The next slides will be the same thing from some other rubyists. It\'ll finish up with ways you can help other programmers experience their own "Aw yeah!" moments when they first experience ruby.'
+        this_section_of_the_talk_page_is_not_empty 'outline', with_content: 'The first slides will cover my "Aw yeah!" moment with Ruby. The next slides will be the same thing from some other rubyists. It\'ll finish up with ways you can help other programmers experience their own "Aw yeah!" moments when they first experience ruby.'
 
-      this_section_of_the_talk_page_is_empty 'why_its_interesting', :with_missing_content_message => 'Nothing about why this talk is interesting has been provided yet.'
-      i_follow_the_link_in_this_section_and_fill_out_the_missing_detail 'why_its_interesting', :with => 'Day to day programming can be dull and enterprisey. We need reminding of why we\'ve chosen Ruby. And we need to be able to pass on that excitement to others.'
-      this_section_of_the_talk_page_is_not_empty 'why_its_interesting', :with_content => 'Day to day programming can be dull and enterprisey. We need reminding of why we\'ve chosen Ruby. And we need to be able to pass on that excitement to others.'
+        this_section_of_the_talk_page_is_empty 'why_its_interesting', with_missing_content_message: 'Nothing about why this talk is interesting has been provided yet.'
+        i_follow_the_link_in_this_section_and_fill_out_the_missing_detail 'why_its_interesting', with: 'Day to day programming can be dull and enterprisey. We need reminding of why we\'ve chosen Ruby. And we need to be able to pass on that excitement to others.'
+        this_section_of_the_talk_page_is_not_empty 'why_its_interesting', with_content: 'Day to day programming can be dull and enterprisey. We need reminding of why we\'ve chosen Ruby. And we need to be able to pass on that excitement to others.'
+      end
+
+      scenario "I can see a list of all the contributors to the talk" do
+        i_can_see_the_list_of_all_the_contributors_to_the_talk
+      end
     end
   end
 
   context "As a visitor" do
-    scenario "When viewing the page about a talk I can see all the details of that talk that have been filled in, and am prompted to sign up or sign in to fill in the missing ones" do
-      visit talk_path(@talk_1)
+    context "When viewing the page about a talk" do
+      setup do
+        visit talk_path(@talk_1)
 
-      the_page_has_title @talk_1.title
+        the_page_has_title @talk_1.title
+      end
 
-      this_section_of_the_talk_page_is_empty 'abstract', but_has_no_flesh_it_out_prompt: true
-      i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section 'abstract'
+      scenario "I can see all the details of that talk that have been filled in, and am prompted to sign up or sign in to fill in the missing ones" do
+        this_section_of_the_talk_page_is_empty 'abstract', but_has_no_flesh_it_out_prompt: true
+        i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section 'abstract'
 
-      this_section_of_the_talk_page_is_empty 'outline', but_has_no_flesh_it_out_prompt: true
-      i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section 'outline'
+        this_section_of_the_talk_page_is_empty 'outline', but_has_no_flesh_it_out_prompt: true
+        i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section 'outline'
 
-      this_section_of_the_talk_page_is_empty 'why_its_interesting', but_has_no_flesh_it_out_prompt: true, with_missing_content_message: 'Nothing about why this talk is interesting has been provided yet.'
-      i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section 'why_its_interesting'
+        this_section_of_the_talk_page_is_empty 'why_its_interesting', but_has_no_flesh_it_out_prompt: true, with_missing_content_message: 'Nothing about why this talk is interesting has been provided yet.'
+        i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section 'why_its_interesting'
+      end
+
+      scenario "I can see a list of all the contributors to the talk" do
+        i_can_see_the_list_of_all_the_contributors_to_the_talk
+      end
     end
   end
+
   def this_section_of_the_talk_page_is_empty(which_section, options = {})
     within("##{which_section}") do
       assert page.has_content?(options[:with_missing_content_message] || "No #{which_section} has been provided yet.")
@@ -105,6 +128,18 @@ class ViewingTalksTest < IntegrationTestCase
   def i_am_prompted_to_sign_up_or_sign_in_to_fill_out_section(which_section)
     within "##{which_section}" do
       there_are_sign_up_and_sign_in_links
+    end
+  end
+
+  def i_can_see_the_list_of_all_the_contributors_to_the_talk
+    within '.contributors' do
+      within '.suggester' do
+        assert page.has_content?(@suggester.email)
+      end
+
+      within '.extra_detail_providers' do
+        assert page.has_content?(@extra_detail_provider.email)
+      end
     end
   end
 end
