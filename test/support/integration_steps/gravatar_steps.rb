4d1b5fb6b65fcedaf681fb45f32e2362869dff34
Tom Stuart <tom@experthuman.com>
2011-03-17 08:33:59 +0000

    Remove unnecessary spans from shared account partial

--- test/support/integration_steps/gravatar_steps.rb
+++ test/support/integration_steps/gravatar_steps.rb
@@ -1,6 +1,6 @@
 module GravatarSteps
   def i_can_see_the_gravatar_for_account(account)
     expected_img_src = Gravatar.gravatar_url(account, :size => 50, :default => 'monsterid')
-    assert has_css?(".avatar img.gravatar[src='#{expected_img_src}']")
+    assert has_css?("img.gravatar[src='#{expected_img_src}']")
   end
 end
