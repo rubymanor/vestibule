e238cfcde6172b599b3433d1ed723094116b5932
Murray Steele <murray.steele@unboxedconsulting.com>
2011-09-01 22:38:15 +0100

    Don't supply a reason when creating the user for testing what happens if we don't have one.

--- test/integration/reason_test.rb
+++ test/integration/reason_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class ReasonTest < IntegrationTestCase
-  context "When signing in after registering" do
+  context "When signing in after registering without having supplied a reason" do
     setup do
-      sign_in Factory(:user)
+      user = Factory(:user, :signup_reason => nil)
+      sign_in user
     end
 
     should "tell the user that they haven't stated a reason for siging up" do
@@ -29,6 +30,17 @@ class ReasonTest < IntegrationTestCase
           assert has_css?("#signup_reason", :text => "I have signed up because I want to help shape the content")
         end
       end
+    end
+  end
+
+  context "When signing in after registering when they have supplied a reason" do
+    setup do
+      user = Factory(:user)
+      sign_in user
+    end
+
+    should "not tell the user that they haven't stated a reason for siging up" do
+      assert !has_content?("Other attendees are wondering what you hope to get out of Ruby Manor.")
     end
   end
 end
