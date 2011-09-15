7d9c707c3958be1e9a554397d4b61384195860e4
Tom Stuart <tom@experthuman.com>
2011-09-15 18:53:16 +0100

    Cast Twitter UID to string before querying database
    Worked fine before on MySQL, but presumably Postgres is pickier.

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
   def self.update_twitter_images
     all.map { |user| user.twitter_uid.to_i }.each_slice(TWITTER_USERS_PER_REQUEST) do |uids|
       Twitter.users(uids).each do |twitter_user|
-        if (user = find_by_twitter_uid(twitter_user.id)).present? && (twitter_image = twitter_user.profile_image_url).present?
+        if (user = find_by_twitter_uid(twitter_user.id.to_s)).present? && (twitter_image = twitter_user.profile_image_url).present?
           user.update_attributes :twitter_image => twitter_image
         end
       end
