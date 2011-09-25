11cd65b9a6b5e9e71f5d7a3179b10590a5fb1ceb
James Adam <james@lazyatom.com>
2011-09-25 20:14:56 +0100

    Another funnel to the motivation entry form.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Vestibule::Application.routes.draw do
   match "/motivation", :to => "home#motivation", :as => "motivation"
   match "/champs", :to => "home#champs", :as => "champs"
   match "/about", :to => "home#about", :as => "about"
-  match "/my_motivation", :to => "home#my_motivation"
+  match "/my_motivation", :to => "home#my_motivation", :as => "complete_motivation"
 
   match "/dashboard", :to => "dashboard#index"
 
