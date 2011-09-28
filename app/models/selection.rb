45c2b361fa630f8f6e00467ed6b589523cea13d2
James Adam <james@lazyatom.com>
2011-09-28 20:19:39 +0100

    UX tweaks.

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -5,10 +5,14 @@ class Selection < ActiveRecord::Base
   validates_presence_of :proposal_id, :user_id
   validate :limited_number_of_selections_per_user
 
+  def self.limit_per_user
+    8
+  end
+
   private
 
   def limited_number_of_selections_per_user
-    if Selection.where(user_id: user.id).count >= 8
+    if Selection.where(user_id: user.id).count >= self.class.limit_per_user
       errors.add(:base, "You can only select 8 proposals at a time")
     end
   end
