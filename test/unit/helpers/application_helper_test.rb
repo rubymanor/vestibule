f7541eef9a2f4afea48dd3cccf3a39b1fcb39224
Richard Drake <rdrake98@gmail.com>
2011-10-03 14:31:50 +0100

    @nickname links to that Vestibule user (not Twitter)

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -27,25 +27,32 @@ class ApplicationHelperTest < ActionView::TestCase
 	  context "for a single user" do
 	    setup do
 	      @user = Factory(:user, :twitter_nickname => "rdrake98", :name => "Richard Drake")
+				@long = link_to("Richard Drake", "/users/rdrake98")
+				@short = link_to("@rdrake98", "/users/rdrake98")
 	    end
 
 	    should "turn into link" do
-	      assert_equal link_to("Richard Drake", "/users/rdrake98"), wikiize("[[Richard Drake]]")
+	      assert_equal @long, wikiize("[[Richard Drake]]")
+	      assert_equal @short, wikiize("@rdrake98")
+	      assert_equal "@other", wikiize("@other")
+	      assert_equal "#{@long}same as #{@short}; #{@short} is but not @other.", wikiize("[[Richard Drake]]same as @rdrake98; @rdrake98 is but not @other.")
+	      assert_equal "#{@long}not same as@rdrake98; #{@short} is.", wikiize("[[Richard Drake]]not same as@rdrake98; @rdrake98 is.")
+				# the following ain't perfect but we can probably live with it
+	      assert_equal "#{@long}same as #{@short};@rdrake98 isn't.", wikiize("[[Richard Drake]]same as @rdrake98;@rdrake98 isn't.")
 	    end
 	  end
 	
 	  context "for a single proposal" do
 	    setup do
 	      @user = Factory(:proposal, :title => "My One and Only")
+				@my_only = link_to("My One and Only", "/proposals/1")
+				@her_latest = link_to("Her Latest Idea", "/proposals/new?title=Her%2520Latest%2520Idea", :class => "New")
 	    end
 
 	    should "turn into link" do
-	      assert_equal link_to("My One and Only", "/proposals/1"), wikiize("[[My One and Only]]")
-	      assert_equal link_to("Her Latest Idea", "/proposals/new?title=Her%2520Latest%2520Idea", :class => "New"), wikiize("[[Her Latest Idea]]")
-	      assert_equal(
-					"I prefer <a href=\"/proposals/new?title=Her%2520Latest%2520Idea\" class=\"New\">Her Latest Idea</a> to <a href=\"/proposals/1\">My One and Only</a>.", 
-					wikiize("I prefer [[Her Latest Idea]] to [[My One and Only]].")
-				)
+	      assert_equal @my_only, wikiize("[[My One and Only]]")
+	      assert_equal @her_latest, wikiize("[[Her Latest Idea]]")
+	      assert_equal "I prefer #{@her_latest} to #{@my_only}.", wikiize("I prefer [[Her Latest Idea]] to [[My One and Only]].")
 	    end
 	  end
 	end
