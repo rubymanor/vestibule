db28c6fd10fd1e1ed1d40e0274b3add9cf59cc80
Richard Drake <rdrake98@gmail.com>
2011-10-03 14:57:07 +0100

    [[]] left unchanged

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -31,10 +31,15 @@ class ApplicationHelperTest < ActionView::TestCase
 				@short = link_to("@rdrake98", "/users/rdrake98")
 	    end
 
+	    should "not turn into link" do
+	      assert_equal "[[]]", wikiize("[[]]")
+				# what about multiple lines inside [[...]]?
+	      assert_equal "@other", wikiize("@other")
+	    end
+
 	    should "turn into link" do
 	      assert_equal @long, wikiize("[[Richard Drake]]")
 	      assert_equal @short, wikiize("@rdrake98")
-	      assert_equal "@other", wikiize("@other")
 	      assert_equal "#{@long}same as #{@short}; #{@short} is but not @other.", wikiize("[[Richard Drake]]same as @rdrake98; @rdrake98 is but not @other.")
 	      assert_equal "#{@long}not same as@rdrake98; #{@short} is.", wikiize("[[Richard Drake]]not same as@rdrake98; @rdrake98 is.")
 				# the following ain't perfect but we can probably live with it
