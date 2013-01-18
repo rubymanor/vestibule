d493ddd38801f8ec32db31423589e2f0c0f3e205
Murray Steele <muz@h-lame.com>
2013-01-18 15:09:42 +0000

    Ran rake simple_form:install --bootstrap
    Not that we're using simple_form_for any more, which explains why our hints and custom labels have gone missing.  Might need to revisit.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -44,48 +44,6 @@ SimpleForm.setup do |config|
     b.use :hint,  :wrap_with => { :tag => :span, :class => :hint }
     b.use :error, :wrap_with => { :tag => :span, :class => :error }
   end
-  config.wrappers :blueprint, :class => :input,
-    :hint_class => :field_with_hint, :error_class => :field_with_errors do |b|
-    ## Extensions enabled by default
-    # Any of these extensions can be disabled for a
-    # given input by passing: `f.input EXTENSION_NAME => false`.
-    # You can make any of these extensions optional by
-    # renaming `b.use` to `b.optional`.
-
-    # Determines whether to use HTML5 (:email, :url, ...)
-    # and required attributes
-    b.use :html5
-
-    # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input :placeholder => "Placeholder"
-    b.use :placeholder
-
-    ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => :lookup`
-    # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable the lookup for any of those
-    # extensions by default, you can change `b.optional` to `b.use`.
-
-    # Calculates maxlength from length validations for string inputs
-    b.optional :maxlength
-
-    # Calculates pattern from format validations for string inputs
-    b.optional :pattern
-
-    # Calculates min and max from length validations for numeric inputs
-    b.optional :min_max
-
-    # Calculates readonly automatically from readonly attributes
-    b.optional :readonly
-
-    ## Inputs
-    b.use :label_input
-    b.use :hint,  :wrap_with => { :tag => :span, :class => :hint }
-    b.wrapper :error_wrapper, :tag => :span, :class => 'error-wrapper' do |ew|
-      ew.use :error, :wrap_with => { :tag => :span, :class => :error }
-    end
-  end
-
   # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :default
 
