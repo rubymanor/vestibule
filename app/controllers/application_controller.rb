e35fffc5d627d2f8c8bb791e8f9461272e1128f3
James Adam <james@lazyatom.com>
2013-01-22 11:18:49 +0000

    Don't raise an exception if the user isn't found.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 end
