4c9010559bdccaec0b23df278b1eadc7eb4f0cc5
Nikos Dimitrakopoulos <nikosd@euruko2013.org>
2013-02-10 12:30:21 +0200

    Don't try to connect to the database on assets precompilation task.
    See https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting for more info.

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,7 @@ module Vestibule
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    config.assets.initialize_on_precompile = false
     config.assets.enabled = true
     config.assets.version = '1.0'
 
