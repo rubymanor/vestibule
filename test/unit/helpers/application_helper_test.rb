b1fe5d6a7814f7d9b12577ca656ba8b25bdbd534
Murray Steele <muz@h-lame.com>
2013-02-18 23:00:24 +0000

    Always have a gravatar even if we don't have an email for the user.

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -5,4 +5,9 @@ class ApplicationHelperTest < ActionView::TestCase
     user = FactoryGirl.build(:user, :email => "alice@example.com")
     assert_equal "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", avatar_url(user)
   end
+
+  test "avatar_url constructs a 0 image using gravatar when no email is present" do
+    user = FactoryGirl.build(:user, :email => nil)
+    assert_equal "http://www.gravatar.com/avatar/0", avatar_url(user)
+  end
 end
