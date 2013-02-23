a5b190f5ac2b07dcdc81644be3592bc3e71ccb4b
Murray Steele <muz@h-lame.com>
2013-02-23 16:48:41 +0000

    Outline what we want to happen on the proposals controller

--- test/integration/proposal_test.rb
+++ test/integration/proposal_test.rb
@@ -57,130 +57,251 @@ class ProposalTest < IntegrationTestCase
       sign_in @user
     end
 
-    context "and I propose a talk with all the required details" do
-      setup { propose_talk :title => "My Amazing Talk", :description => 'This talk is amazing.' }
+    context 'and the app is in "cfp" mode' do
+      setup { Vestibule.mode_of_operation = :cfp }
 
-      should "be able to see my proposal on the site" do
-        visit proposals_path
-        click_link "My Amazing Talk"
-        assert_page_has_proposal \
-          :title        => "My Amazing Talk",
-          :description  => 'This talk is amazing.',
-          :proposer     => @user
+      context "and I propose a talk with all the required details" do
+        setup { propose_talk :title => "My Amazing Talk", :description => 'This talk is amazing.' }
+
+        should "be able to see my proposal on the site" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          assert_page_has_proposal \
+            :title        => "My Amazing Talk",
+            :description  => 'This talk is amazing.',
+            :proposer     => @user
+        end
+
+        should "be able to withdraw my proposal" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          click_button "Withdraw proposal"
+
+          assert_proposal_withdrawn "My Amazing Talk"
+        end
+
+        context "and then edit my proposal" do
+          setup do
+            visit proposals_path
+            click_link "My Amazing Talk"
+            click_link "Edit proposal"
+          end
+
+          context "with valid content" do
+            setup do
+              fill_in "Title", :with => "My Even More Amazing Talk"
+              fill_in "Description", :with => "This talk is wildly amazing."
+              click_button "Update proposal"
+            end
+
+            should "update the proposal" do
+              visit proposals_path
+              click_link "My Even More Amazing Talk"
+              assert_page_has_proposal \
+                :title        => "My Even More Amazing Talk",
+                :description  => 'This talk is wildly amazing.',
+                :proposer     => @user
+            end
+          end
+
+          context "and remove the title" do
+            setup do
+              fill_in "Title", :with => ""
+              click_button "Update proposal"
+            end
+
+            should "alert me that the title is required" do
+              i_am_warned_about Proposal, :title, "can't be blank"
+            end
+          end
+        end
       end
 
-      should "be able to withdraw my proposal" do
-        visit proposals_path
-        click_link "My Amazing Talk"
-        click_button "Withdraw proposal"
-
-        assert_proposal_withdrawn "My Amazing Talk"
-      end
-
-      context "and then edit my proposal" do
+      context "and I propose a talk with markdown-formatted description" do
         setup do
+          propose_talk :title => "My Amazing Talk", :description => %{
+  # A moment in time
+
+  blah blah blah
+          }.strip
+        end
+
+        should "be able to see my proposal with a formatted description" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          within_proposal do
+            within(".description") do
+              assert page.has_css?('h1', :text => 'A moment in time')
+              assert page.has_css?('p', :text => 'blah blah blah')
+            end
+          end
+        end
+      end
+
+      context "and I propose a talk without a title" do
+        setup { propose_talk :title => nil }
+
+        should "alert me that the title is required" do
+          i_am_warned_about Proposal, :title, "can't be blank"
+        end
+      end
+
+      context "and a proposal from another user exists" do
+        setup do
+          @other_persons_proposal = FactoryGirl.create(:proposal, :title => "Another talk")
+        end
+
+        should "not see a link to edit that proposal" do
+          visit proposals_path
+          click_link "Another talk"
+          assert !page.has_content?("Edit proposal"), "link to edit proposal should not be present"
+        end
+
+        should "not be able to edit that proposal" do
+          visit edit_proposal_path(@other_persons_proposal)
+          i_am_alerted "You cannot edit proposals that are owned by other users"
+        end
+      end
+
+      context "and there are some proposals (mine and others)" do
+        setup do
+          propose_talk :title => "My Amazing Talk", :description => %{
+  # A moment in time
+
+  blah blah blah
+          }.strip
+          @other_persons_proposal = FactoryGirl.create(:proposal, :title => "Another talk", :created_at => 3.days.ago)
+        end
+
+        should "be able to subscribe to the rss feed of proposals" do
+          visit proposals_path
+          assert page.has_css?("link[rel='alternate'][type='application/rss+xml'][href$='#{proposals_path(:format => :rss)}']")
+          visit proposals_path(:format => :rss)
+          assert_match %r{application/rss\+xml}, page.response_headers['Content-Type']
+        end
+
+        should 'have my talk and the other talk in the feed, in newest first order' do
+          visit proposals_path(:format => :rss)
+          assert page.has_xpath?('.//item[position() = 2]/title', :text => @other_persons_proposal.title)
+          assert page.has_xpath?('.//item[position() = 1]/title', :text => "My Amazing Talk")
+        end
+      end
+    end
+
+    context 'and the app is in "voting mode"' do
+      setup { Vestibule.mode_of_operation = :voting }
+
+      should "not see a link to propose a talk" do
+        visit proposals_path
+        assert !page.has_content?("Propose talk"), "link to propose talk should not be present!"
+      end
+
+      should "not be able to propose a talk" do
+        visit new_proposal_path
+        i_am_alerted("In voting mode you cannot make a proposal")
+      end
+
+      context 'and I have a proposal of my own' do
+        setup { FactoryGirl.create(:proposal, title: 'My Amazing Talk', proposer: @user) }
+
+        should "be able to withdraw my proposal" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          click_button "Withdraw proposal"
+
+          assert_proposal_withdrawn "My Amazing Talk"
+        end
+
+        should "be able to edit my proposal" do
           visit proposals_path
           click_link "My Amazing Talk"
           click_link "Edit proposal"
-        end
-
-        context "with valid content" do
-          setup do
-            fill_in "Title", :with => "My Even More Amazing Talk"
-            fill_in "Description", :with => "This talk is wildly amazing."
-            click_button "Update proposal"
-          end
-
-          should "update the proposal" do
-            visit proposals_path
-            click_link "My Even More Amazing Talk"
-            assert_page_has_proposal \
-              :title        => "My Even More Amazing Talk",
-              :description  => 'This talk is wildly amazing.',
-              :proposer     => @user
-          end
-        end
-
-        context "and remove the title" do
-          setup do
-            fill_in "Title", :with => ""
-            click_button "Update proposal"
-          end
-
-          should "alert me that the title is required" do
-            i_am_warned_about Proposal, :title, "can't be blank"
-          end
+          fill_in "Title", :with => "My Even More Amazing Talk"
+          fill_in "Description", :with => "This talk is wildly amazing."
+          click_button "Update proposal"
+          visit proposals_path
+          click_link "My Even More Amazing Talk"
+          assert_page_has_proposal \
+            :title        => "My Even More Amazing Talk",
+            :description  => 'This talk is wildly amazing.',
+            :proposer     => @user
         end
       end
     end
 
-    context "and I propose a talk with markdown-formatted description" do
-      setup do
-        propose_talk :title => "My Amazing Talk", :description => %{
-# A moment in time
+    context 'and the app is in "agenda mode"' do
+      setup { Vestibule.mode_of_operation = :agenda }
 
-blah blah blah
-        }.strip
-      end
-
-      should "be able to see my proposal with a formatted description" do
+      should "not see a link to propose a talk" do
         visit proposals_path
-        click_link "My Amazing Talk"
-        within_proposal do
-          within(".description") do
-            assert page.has_css?('h1', :text => 'A moment in time')
-            assert page.has_css?('p', :text => 'blah blah blah')
-          end
+        assert !page.has_content?("Propose talk"), "link to propose talk should not be present!"
+      end
+
+      should "not be able to propose a talk" do
+        visit new_proposal_path
+        i_am_alerted("In agenda mode you cannot make a proposal")
+      end
+
+      context 'and I have a proposal of my own' do
+        setup { FactoryGirl.create(:proposal, title: 'My Amazing Talk', proposer: @user) }
+
+        should "be able to withdraw my proposal" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          click_button "Withdraw proposal"
+
+          assert_proposal_withdrawn "My Amazing Talk"
+        end
+
+        should "be able to edit my proposal" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          click_link "Edit proposal"
+          fill_in "Title", :with => "My Even More Amazing Talk"
+          fill_in "Description", :with => "This talk is wildly amazing."
+          click_button "Update proposal"
+          visit proposals_path
+          click_link "My Even More Amazing Talk"
+          assert_page_has_proposal \
+            :title        => "My Even More Amazing Talk",
+            :description  => 'This talk is wildly amazing.',
+            :proposer     => @user
         end
       end
     end
 
-    context "and I propose a talk without a title" do
-      setup { propose_talk :title => nil }
+    context 'and the app is in "archive mode"' do
+      setup { Vestibule.mode_of_operation = :archive }
 
-      should "alert me that the title is required" do
-        i_am_warned_about Proposal, :title, "can't be blank"
-      end
-    end
-
-    context "and a proposal from another user exists" do
-      setup do
-        @other_persons_proposal = FactoryGirl.create(:proposal, :title => "Another talk")
-      end
-
-      should "not see a link to edit that proposal" do
+      should "not see a link to propose a talk" do
         visit proposals_path
-        click_link "Another talk"
-        assert !page.has_content?("Edit proposal"), "link to edit proposal should not be present"
+        assert !page.has_content?("Propose talk"), "link to propose talk should not be present!"
       end
 
-      should "not be able to edit that proposal" do
-        visit edit_proposal_path(@other_persons_proposal)
-        i_am_alerted "You cannot edit proposals that are owned by other users"
-      end
-    end
-
-    context "and there are some proposals (mine and others)" do
-      setup do
-        propose_talk :title => "My Amazing Talk", :description => %{
-# A moment in time
-
-blah blah blah
-        }.strip
-        @other_persons_proposal = FactoryGirl.create(:proposal, :title => "Another talk", :created_at => 3.days.ago)
+      should "not be able to propose a talk" do
+        visit new_proposal_path
+        i_am_alerted("In archive mode you cannot make a proposal")
       end
 
-      should "be able to subscribe to the rss feed of proposals" do
-        visit proposals_path
-        assert page.has_css?("link[rel='alternate'][type='application/rss+xml'][href$='#{proposals_path(:format => :rss)}']")
-        visit proposals_path(:format => :rss)
-        assert_match %r{application/rss\+xml}, page.response_headers['Content-Type']
-      end
+      context 'and I have a proposal of my own' do
+        setup { FactoryGirl.create(:proposal, title: 'My Amazing Talk', proposer: @user) }
 
-      should 'have my talk and the other talk in the feed, in newest first order' do
-        visit proposals_path(:format => :rss)
-        assert page.has_xpath?('.//item[position() = 2]/title', :text => @other_persons_proposal.title)
-        assert page.has_xpath?('.//item[position() = 1]/title', :text => "My Amazing Talk")
+        should "not see a link to withdraw my proposal" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          assert !page.has_content?("Withdraw proposal")
+        end
+
+        should "not see a link to edit my proposal" do
+          visit proposals_path
+          click_link "My Amazing Talk"
+          assert !page.has_content?("Edit proposal"), "link to edit proposal should not be present"
+        end
+
+        should "not be able to edit my proposal" do
+          visit edit_proposal_path(Proposal.find_by_title('My Amazing Talk'))
+          i_am_alerted "In archive mode you cannot change a proposal"
+        end
       end
     end
   end
