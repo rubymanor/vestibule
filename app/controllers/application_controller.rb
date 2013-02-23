9ad0e0777d9aca09dd1aac4f3cfb5d3667e48614
Murray Steele <muz@h-lame.com>
2013-02-23 19:09:18 +0000

    Add a global banner if we are in archive mode.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :user_signed_in?, :can?
+  before_filter :set_archive_mode_warning_if_required
 
   private
 
@@ -22,5 +23,11 @@ class ApplicationController < ActionController::Base
 
   def can?(action, object)
     Vestibule.mode_of_operation.can?(action, object)
+  end
+
+  def set_archive_mode_warning_if_required
+    if Vestibule.mode_of_operation.mode == :archive
+      flash.now[:archive] = 'This version of Vestibule is <em>read only</em>. It represents an archive of the community effort to produce content for <a href="http://rubymanor.org/4/">Ruby Manor 4</a>.'.html_safe
+    end
   end
 end
