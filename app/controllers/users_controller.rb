d1bca0581659e39f69974aa59c6933e3950b237e
Nikos Dimitrakopoulos <nikosd@euruko2013.org>
2013-03-19 19:57:02 +0200

    Migrating authorization logic out of UsersController. Refs #5.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except => [:show]
-  before_filter :load_user
-  before_filter :restrict_editing, :only => [:edit, :update]
+  load_and_authorize_resource
 
   def show
   end
@@ -10,22 +8,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update_attributes(params[:user])
+    if @user.update_attributes(params[:user])
       flash[:notice] = "Successfully updated user."
     end
-    redirect_to user_path(current_user)
-  end
 
-  private
-
-  def load_user
-    @user = User.find(params[:id])
-  end
-
-  def restrict_editing
-    if @user != current_user
-      flash[:alert] = "You cannot update another user"
-      redirect_to user_path(current_user)
-    end
+    redirect_to user_path(@user)
   end
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   def setup
     @user = FactoryGirl.create(:user)
+    @other_user = FactoryGirl.create(:user)
   end
 
   context "When logged in" do
@@ -17,29 +18,44 @@ class UsersControllerTest < ActionController::TestCase
         end
 
         should assign_to(:user) { @user }
+        should respond_with(:success)
         should render_template(action)
       end
     end
 
-    context "on #PUT to update with a signup_reason" do
+    context "on #PUT to update self" do
       setup do
         put :update, :id => @user.to_param, :user => {:signup_reason => "Because I want to learn"}
       end
-      should set_the_flash.to("Successfully updated user.")
 
+      should set_the_flash.to("Successfully updated user.")
       should "update the signup reason" do
         assert_equal "Because I want to learn", @user.reload.signup_reason
       end
+      should respond_with(:redirect)
     end
 
-    context "on #PUT to update for another reason" do
+    context 'on #GET to edit for another user' do
       setup do
-        other_user = FactoryGirl.create(:user)
-        put :update, :id => other_user.to_param, :user => {:signup_reason => "Haxx'd"}
+        get :edit, :id => @other_user.to_param
       end
 
+      should assign_to(:user) { @other_user }
       should respond_with(:redirect)
-      should set_the_flash.to(/You cannot update another user/)
+      should set_the_flash.to(/You are not authorized to access this page/)
+    end
+
+    context "on #PUT to update for another user" do
+      setup do
+        put :update, :id => @other_user.to_param, :user => {:signup_reason => "Haxx'd"}
+      end
+
+      should assign_to(:user) { @other_user }
+      should respond_with(:redirect)
+      should set_the_flash.to(/You are not authorized to access this page/)
+      should "not update the signup reason" do
+        assert_not_equal "Haxx'd", @other_user.reload.signup_reason
+      end
     end
   end
 
@@ -51,16 +67,31 @@ class UsersControllerTest < ActionController::TestCase
         end
 
         should assign_to(:user) { @user }
+        should respond_with(:success)
         should render_template(action)
       end
     end
-    [:edit, :update].each do |action|
-      context "on #GET to #{action.to_s}" do
-        setup do
-          get action, :id => @user.to_param
-        end
-        should respond_with(:redirect)
-        should set_the_flash.to(/You need to sign in/)
+
+    context 'on #GET to edit a user' do
+      setup do
+        get :edit, :id => @user.to_param
+      end
+
+      should assign_to(:user) { @user }
+      should respond_with(:redirect)
+      should set_the_flash.to(/You are not authorized to access this page/)
+    end
+
+    context "on #PUT to update a user" do
+      setup do
+        put :update, :id => @user.to_param, :user => {:signup_reason => "Haxx'd"}
+      end
+
+      should assign_to(:user) { @user }
+      should respond_with(:redirect)
+      should set_the_flash.to(/You are not authorized to access this page/)
+      should "not update the signup reason" do
+        assert_not_equal "Haxx'd", @user.reload.signup_reason
       end
     end
   end
