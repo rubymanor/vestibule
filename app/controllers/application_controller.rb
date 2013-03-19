c6dac1614b185b8f0b644a336260ea031ae16d6f
Nikos Dimitrakopoulos <nikosd@euruko2013.org>
2013-03-19 19:55:58 +0200

    Generic authorization error handling. Refs #5.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
 
   before_filter :reload_settings if Rails.env.development?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = exception.message
+    redirect_to(request.referer.presence || root_url)
+  end
+
   private
 
   def reload_settings
