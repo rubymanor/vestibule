27e57dab816874e3c82c10cd8324bee08ca6e818
Matt Patterson <matt@reprocessed.org>
2013-11-29 11:43:56 +0100

    Allow default mode to be set

--- app/models/modes.rb
+++ app/models/modes.rb
@@ -18,11 +18,18 @@ class Modes
     @rule_sets = {}
   end
 
+  def default(name = nil)
+    return rules(@default) if name.nil?
+    @default = name.to_sym
+  end
+
   private
 
   def mode(name, &block)
     rules = Modality::Rules.new([])
     rules.define(&block) if block
     @rule_sets[name.to_sym] = rules
+    @default = name.to_sym unless @default
+    nil
   end
 end
--- test/unit/modes_test.rb
+++ test/unit/modes_test.rb
@@ -33,6 +33,21 @@ class ModesTest < ActiveSupport::TestCase
       assert @modes.rules(:test_mode).can?(:see, :stuff)
     end
 
+    should "allow the definition of multiple modes" do
+      @modes.define do
+        mode :m1 do
+          can :see, :stuff
+        end
+
+        mode :m2 do
+          can :do, :stuff
+        end
+      end
+
+      assert @modes.rules(:m1).can?(:see, :stuff)
+      assert @modes.rules(:m2).can?(:do, :stuff)
+    end
+
     should "return nil from the define method" do
       assert_nil @modes.define { mode :test_mode }
     end
@@ -55,6 +70,26 @@ class ModesTest < ActiveSupport::TestCase
 
     should "return NoRules if asked for a non-existing rule set" do
       refute @modes.rules(:test_mode).can?(:see, :stuff)
+    end
+
+    context "default mode" do
+      should "allow a default to be set" do
+        @modes.define do
+          mode(:m1) { can(:see, :stuff) }
+          mode(:m2) { can(:do, :stuff) }
+
+          default :m2
+        end
+        assert @modes.default.can?(:do, :stuff)
+      end
+
+      should "set the first-defined mode as default if it's not explicit" do
+        @modes.define do
+          mode(:m1) { can(:see, :stuff) }
+          mode(:m2) { can(:do, :stuff) }
+        end
+        assert @modes.default.can?(:see, :stuff)
+      end
     end
   end
 
